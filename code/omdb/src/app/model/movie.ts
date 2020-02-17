d3d2c529f792a56f2fc5ba86f0b430be06d0dc42
guilhermewm <guilherme.webber@outlook.com>
2020-02-16 21:05:51 -0300

    update/model -> adding some new movies attributes

--- code/omdb/src/app/model/movie.ts
+++ code/omdb/src/app/model/movie.ts
@@ -3,7 +3,33 @@ export interface Movie {
     Year: string;
     imdbID: string;
     Type: string;
-    Poster: string;   
+    Poster: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string | string[];
+    Director: string | string[];
+    Writer: string | string[];
+    Actors: string | string[];
+    Plot: string;
+    Language: string | string[];
+    Country: string;
+    Awards: string;
+    Ratings: Rating[];
+    Metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    DVD: string;
+    BoxOffice: string;
+    Production: string | string[];
+    Website: string;
+    Response: string; 
+    Error: string; 
+}
+
+export interface Rating {      
+    Source: string;
+    Value: string;
 }
 
 export interface MovieResponse {
