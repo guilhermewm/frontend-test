94dff15535ab32adcdbe5244c140aa5731012c68
guilhermewm <guilherme.webber@outlook.com>
2020-02-16 21:03:56 -0300

    update/movies-list -> removing some logics and functions

--- code/omdb/src/app/movies/movies-list/movies-list.component.ts
+++ code/omdb/src/app/movies/movies-list/movies-list.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { MoviesService } from 'src/app/services/movies.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Movie } from 'src/app/model/movie';
-import { SearchComponent } from 'src/app/custom-header/search/search.component';
-import { SharedVarService } from 'src/app/custom-header/search/sharedvar';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-movies-list',
@@ -11,69 +9,18 @@ import { SharedVarService } from 'src/app/custom-header/search/sharedvar';
 })
 export class MoviesListComponent implements OnInit {
 
-  constructor(private moviesService: MoviesService, private service: SharedVarService) { }
+  constructor(
+    private location: Location
+  ) { }
 
-  movies: Movie[] = [];
-  filter = 'batman';
-  error: any = { 
-    type: 'Error',
-    message: 'Type on the search input to find your movie. OBS: type the title of the movie'
-  };
-  loading: boolean = false;
-  flagFirstTime = true;
-
-  titleString = '';
-  lastTitleString = '';
-  count: number = 0;
-
+  @Input() movies: Movie[] = [];
+  @Output() movieSelected = new EventEmitter<Movie>(); 
+  
   ngOnInit(): void {
-    this.subscribeToSearch();
-    this.flagFirstTime = false;
   }
 
-  loadMovies() {
-    this.loading = true;
-    this.error = false;
-    if (this.lastTitleString === this.titleString) {
-      this.count++;
-    } else {
-      this.movies = [];
-      this.count = 1;
-    }
-    this.moviesService.listMoviesByTitle(this.titleString, this.count).then(movieResponse => {
-      if (movieResponse.Error) {
-        this.error = {
-          type: "Error",
-          message: this.titleString === '' ? 'Type on the search input to find your movie. OBS: type the title of the movie' : movieResponse.Error
-        };
-        this.movies = [];
-      } else {
-        let offset = 0;
-        movieResponse.Search.forEach(movie => {
-          setTimeout(()=>{
-            this.movies.push(movie);
-          }, offset);
-          offset += 300;
-        });
-        this.lastTitleString = this.titleString;
-        this.error = {};
-      }
-    }).catch(e => {
-      this.error = {
-        type: e.name,
-        message: e.message
-      }
-    }).finally(()=> {      
-      this.loading = false;
-    });  
-  }
-
-  subscribeToSearch() {
-    this.service.getValue().subscribe((titleString) => {
-      if (!this.flagFirstTime) {
-        this.titleString = titleString;
-        this.loadMovies();
-      }
-    })
+  openMovie(movie: Movie) {    
+    this.location.replaceState(`/movies/${movie.imdbID}`);
+    this.movieSelected.emit(movie);
   }
 }
